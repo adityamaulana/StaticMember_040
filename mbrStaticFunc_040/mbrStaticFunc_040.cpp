79815fc76a65490cf7a9ef3efd8dc50fc16b8b90
adityamaulana <aditmau26@gmail.com>
2023-05-15 18:31:42 +0700

    sec comm

--- mbrStaticFunc_040/mbrStaticFunc_040.cpp
+++ mbrStaticFunc_040/mbrStaticFunc_040.cpp
@@ -24,4 +24,21 @@ public:
 	{
 		return nim;
 	}
+
+	Student(string pname):name(pname){setId();}
 };
+
+int Student::nim=0;
+
+void Student::setId()
+{
+	id = ++nim;
+}
+
+void Student::printAll()
+{
+	cout<<"ID = "<<id<<endl;
+	cout<<"Name = "<<name<<endl;
+
+	cout<<endl;
+}
