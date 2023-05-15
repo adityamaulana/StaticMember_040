e40405239ec2344fa360ff236573910a692c00b6
adityamaulana <aditmau26@gmail.com>
2023-05-15 18:24:54 +0700

    fourth comm printing

--- Constructor_040/Constructor_040.cpp
+++ Constructor_040/Constructor_040.cpp
@@ -39,5 +39,24 @@ Student::Student(int iNim, string iName)
 	name = iName;
 }
 
+void Student::print()
+{
+	cout<<endl<< "Nim ="<<nim<<endl;
+	cout<< " Name = "<<name<<endl;
+}
 
+int main()
+{
+	Student Mhs1;
+	Student Mhs2;
+	Student Mhs3("Coki Pardede");
+	Student Mhs4("Tretan Muslim");
+
+	Mhs1.print();
+	Mhs2.print();
+	Mhs3.print();
+	Mhs4.print();
+
+	return 0;
+}
 
